3a2ef00ff5735e998a1aff945857b7ab838b982f
roshan ahamed <roshanahamed9600@gmail.com>
2024-06-22 00:41:02 +0530

    Add n

--- src/components/Clientpart.js
+++ src/components/Clientpart.js
@@ -1,11 +1,10 @@
-'use client'
+"use client";
 import React, { useEffect } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { EmblaCarouselAutoplay } from "embla-carousel-autoplay";
 import { accenture, anand, manpho, president, jain } from "@/assets/images";
 import Client from "./Clients";
 import Autoplay from "embla-carousel-autoplay";
-
 
 const clients = [
   {
@@ -31,21 +30,28 @@ const clients = [
 ];
 
 const Clientpart = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true },[Autoplay()]);
-
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [Autoplay()]);
+  
+  useEffect(() => {
+    if (emblaApi) {
+      emblaApi.reInit();
+    }
+  }, [emblaApi]);
 
   return (
     <div className="py-4">
-      <h2 className="font-merriweather text-xl font-bold text-center">Our Clients</h2>
-    <div className="embla m-4" ref={emblaRef}>
-      <div className="embla__container flex flex-row items-center justify-around items-center justify-center">
-        {clients.map((client, index) => (
-          <div className="embla__slide px-2" key={index}>
-            <Client {...client} />
-          </div>
-        ))}
+      <h2 className="font-merriweather text-xl font-bold text-center">
+        Our Clients
+      </h2>
+      <div className="embla m-4" ref={emblaRef}>
+        <div className="embla__container flex items-center justify-center">
+          {clients.map((client, index) => (
+            <div className="embla__slide px-2 min-w-[80%]" key={index}>
+              <Client {...client} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
