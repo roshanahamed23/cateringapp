506b9267bb0e779362ebad90015427bd2bd5db08
roshan ahamed <roshanahamed9600@gmail.com>
2024-06-22 00:34:26 +0530

    Add new

--- src/components/Clients.js
+++ src/components/Clients.js
@@ -7,7 +7,7 @@ const Client = ({image,text,link}) => {
   const href=link?link:"#";
   return (
     <Link href={href}>
-    <div className='flex transition ease-in-out duration-300 transform hover:scale-90 flex-row items-center justify-center gap-4 px-28 py-20 max-w-sm rounded-lg bg-white border border-gray-400 shadow-md'>
+    <div className='flex transition ease-in-out duration-300 transform hover:scale-90 flex-col~ items-center justify-center gap-4 px-28 py-20 max-w-sm rounded-lg bg-white border border-gray-400 shadow-md'>
         <div className='rounded-md flex justify-center items-center w-36 h-36 relative'>
         <Image src={image}
         style={{ objectFit: 'cover' }}
